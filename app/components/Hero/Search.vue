fc0cd901466edd5e4187a390055ebac999d041f3
j0u1 <158160207+j0u1@users.noreply.github.com>
2026-03-30 18:50:36 +0300

    fix(Search): fix searching

--- app/components/Hero/Search.vue
+++ app/components/Hero/Search.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
 import { SearchIcon, XIcon } from "@lucide/vue";
 
-const search = defineModel({ type: String });
+const search = defineModel({ type: String, default: "" });
 </script>
 
 <template>
     <div class="flex items-center outline outline-secondary bg-bg rounded-xl py-2 px-3 gap-2.5 w-full max-w-md focus-within:outline-accent duration-200 transition-all">
-        <SearchIcon class="size-4.5 text-additional" />
+        <SearchIcon class="size-4 text-additional" />
         <input type="search" v-model="search" placeholder="Search Dockerfile..." class="w-full outline-0" inputmode="search" autofocus />
         <XIcon @click="search = ''" class="size-4 text-additional duration-200 transition-all" :class="search ? 'cursor-pointer scale-100' : 'cursor-default scale-0'" />
     </div>
--- app/components/Hero/index.vue
+++ app/components/Hero/index.vue
@@ -1,9 +1,11 @@
+<script setup>
+const search = defineModel({ type: String });
+</script>
+
 <template>
-    <main class="flex flex-col items-center justify-center gap-6 pt-24 pb-12">
-        <div class="space-y-4">
-            <HeroTitle />
-            <p class="text-additional">Dockerfile snippets for popular frameworks and tools</p>
-        </div>
-        <HeroSearch />
+    <main class="flex flex-col items-center justify-center gap-4 pt-24 pb-12">
+        <HeroTitle />
+        <p class="text-additional">Dockerfile snippets for popular frameworks and tools</p>
+        <HeroSearch v-model="search" />
     </main>
 </template>
