a8ec33dde1a8b146b1ad58e2a16d6e8697f62d15
j0u1 <158160207+j0u1@users.noreply.github.com>
2026-03-30 19:06:58 +0300

    chore(search): add debounce

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts">
+import { refDebounced } from "@vueuse/core";
+
 const search = ref("");
+const debouncedSearch = refDebounced(search, 200);
 </script>
 
 <template>
     <Hero v-model="search" />
-    <Files :search="search" />
+
+    <Files :search="debouncedSearch" />
 </template>
