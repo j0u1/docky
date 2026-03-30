2adf78c044ed179b030835f0185efd3c79d67a6d
j0u1 <158160207+j0u1@users.noreply.github.com>
2026-03-30 19:17:45 +0300

    chore(hero): add screen reader text

--- app/components/Hero/Title.vue
+++ app/components/Hero/Title.vue
@@ -3,6 +3,7 @@
         <h1 class="text-3xl font-medium">🐋 Docky</h1>
         <NuxtLink class="mt-1.5 duration-300 transition-all text-additional hover:text-accent" to="https://github.com/j0u1/docky" target="_blank">
             <IconsGithub class="size-5" />
+            <span class="sr-only">View source on GitHub</span>
         </NuxtLink>
     </div>
 </template>
