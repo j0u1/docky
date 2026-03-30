da8feee8a822f21bf6643ab0ee1a86c8fba3545e
j0u1 <158160207+j0u1@users.noreply.github.com>
2026-03-30 17:47:41 +0300

    feat(hero): add Github link

--- app/components/Hero/Title.vue
+++ app/components/Hero/Title.vue
@@ -1,6 +1,5 @@
 <template>
     <div class="relative flex flex-col">
-        <NuxtLink class="absolute -top-5 left-11.25 text-additional text-sm hover:text-accent duration-300 transition-all" to="https://d1yy.ru" target="_blank">by diy</NuxtLink>
         <div class="flex items-center gap-2">
             <h1 class="text-3xl font-medium">🐋 Docky</h1>
             <NuxtLink class="mt-1.5 duration-300 transition-all text-additional hover:text-accent" to="https://github.com/j0u1/docky" target="_blank">
--- app/components/Hero/index.vue
+++ app/components/Hero/index.vue
@@ -5,12 +5,12 @@ const search = defineModel({ type: String });
 </script>
 
 <template>
-    <main class="flex flex-col items-center justify-center gap-4 pt-12 pb-8">
+    <main class="flex flex-col items-center justify-center gap-4 pt-24 pb-12">
         <HeroTitle />
         <p class="text-additional">Dockerfile snippets for popular frameworks and tools</p>
         <div class="flex items-center outline outline-secondary bg-bg rounded-xl py-2 px-3 gap-2.5 w-full max-w-md focus-within:outline-accent duration-200 transition-all">
             <SearchIcon class="size-4 text-additional" />
-            <input type="search" v-model="search" placeholder="Search..." class="w-full outline-0" inputmode="search" autofocus />
+            <input type="search" v-model="search" placeholder="Search Dockerfile..." class="w-full outline-0" inputmode="search" autofocus />
             <XIcon @click="search = ''" class="size-4 text-additional duration-200 transition-all" :class="search ? 'cursor-pointer scale-100' : 'cursor-default scale-0'" />
         </div>
     </main>
