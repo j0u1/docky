13a3ac0537fb3ba3850ce684e13cf0e8bfaecfff
j0u1 <158160207+j0u1@users.noreply.github.com>
2026-03-31 20:34:36 +0300

    style(app.vue): change transition class

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container mx-auto px-4 lg:px-8 duration-200 transition-transform flex flex-col gap-8 pb-8">
+  <div class="container mx-auto px-4 lg:px-8 duration-300 transition-all flex flex-col gap-8 pb-8">
     <NuxtRouteAnnouncer class="absolute" />
     <NuxtPage />
     <Footer />
