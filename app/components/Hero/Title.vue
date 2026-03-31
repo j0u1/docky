af41aee82068c38d261a7c6ebbca4d6e1ad0ed92
j0u1 <158160207+j0u1@users.noreply.github.com>
2026-03-31 20:17:16 +0300

    chore(hero): remove protocol from URLs

--- app/components/Hero/Title.vue
+++ app/components/Hero/Title.vue
@@ -1,11 +1,7 @@
 <template>
   <div class="flex items-center gap-2 justify-center">
     <h1 class="text-3xl font-medium">🐋 Docky</h1>
-    <NuxtLink
-      class="mt-1.5 duration-300 transition-all text-additional hover:text-accent"
-      to="https://github.com/j0u1/docky"
-      target="_blank"
-    >
+    <NuxtLink class="mt-1.5 duration-300 transition-all text-additional hover:text-accent" to="//github.com/j0u1/docky" target="_blank">
       <IconsGithub class="size-5" />
       <span class="sr-only">View source on GitHub</span>
     </NuxtLink>
--- docs/README_ru.md
+++ docs/README_ru.md
@@ -2,7 +2,7 @@
 
 **Docky** — легковесный поисковик по готовым Docker-конфигурациям для любых стеков через поиск. Забудьте о копировании кривых конфигов из ИИ.
 
-<img width="1920" height="949" alt="изображение" src="https://github.com/user-attachments/assets/dcc546e4-0c5c-4152-8258-37e8dae07378" />
+<img width="1920" height="949" alt="изображение" src="//github.com/user-attachments/assets/dcc546e4-0c5c-4152-8258-37e8dae07378" />
 
 ---
 
