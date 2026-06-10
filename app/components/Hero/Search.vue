a6910898593b1798799002f952d613cb9822630b
j0u1 <158160207+j0u1@users.noreply.github.com>
2026-06-10 23:40:16 +0300

    style(Search.vue): enhance styling for SearchIcon and input elements

--- app/components/Hero/Search.vue
+++ app/components/Hero/Search.vue
@@ -6,21 +6,11 @@ const search = defineModel({ type: String, default: "" });
 
 <template>
   <div
-    class="flex items-center outline outline-secondary bg-bg rounded-xl py-2 px-3 gap-2.5 w-full max-w-md focus-within:outline-accent duration-200 transition-all"
-  >
-    <SearchIcon class="size-4 text-accent" />
-    <input
-      type="search"
-      v-model="search"
-      placeholder="Search Dockerfile..."
-      class="w-full outline-0"
-      inputmode="search"
-      autofocus
-    />
-    <XIcon
-      @click="search = ''"
-      class="size-4 text-additional duration-200 transition-all"
-      :class="search ? 'cursor-pointer scale-100' : 'cursor-default scale-0'"
-    />
+    class="group flex items-center outline outline-secondary bg-bg rounded-xl py-2 px-3 gap-2.5 w-full max-w-md focus-within:outline-accent duration-200 transition-all">
+    <SearchIcon class="size-4 text-additional group-focus-within:text-accent duration-200 transition-all" />
+    <input type="search" v-model="search" placeholder="Search Dockerfile..." class="w-full outline-0" inputmode="search"
+      autofocus />
+    <XIcon @click="search = ''" class="size-4 text-additional duration-200 transition-all"
+      :class="search ? 'cursor-pointer scale-100' : 'cursor-default scale-0'" />
   </div>
 </template>
