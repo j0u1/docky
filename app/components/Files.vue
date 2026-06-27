d28382caba457d625363a146b9b4d82a79f5c808
j0u1 <158160207+j0u1@users.noreply.github.com>
2026-06-28 00:33:27 +0300

    style(Files.vue): replace div elements with section for semantic markup

--- app/components/Files.vue
+++ app/components/Files.vue
@@ -24,11 +24,11 @@ const onCopied = () => {
 </script>
 
 <template>
-  <div v-if="filteredFiles.length" class="grid grid-cols-1 md:grid-cols-2 duration-300 transition-all gap-4 lg:gap-6">
+  <section v-if="filteredFiles.length" class="grid grid-cols-1 md:grid-cols-2 duration-300 transition-all gap-4 lg:gap-6">
     <File v-for="(file, index) in filteredFiles" :key="index" :file="file" :copied="copied" @copied="onCopied" />
-  </div>
-  <div v-else class="flex items-center justify-center">
+  </section>
+  <section v-else class="flex items-center justify-center">
     <p class="mx-auto text-additional">No such Dockerfile here</p>
-  </div>
+  </section>
   <Toast :isCopy="copied" />
 </template>
