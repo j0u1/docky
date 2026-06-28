b15995b7ca027c6bc1c1981adcc1776ef67c4edf
j0u1 <158160207+j0u1@users.noreply.github.com>
2026-06-28 13:36:27 +0300

    fix(toast): remove unused CheckIcon import and adjust Toast component's z-index

--- app/components/Toast.vue
+++ app/components/Toast.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
-import { CheckIcon, CopyIcon } from "@lucide/vue";
+import { CopyIcon } from "@lucide/vue";
 
 defineProps<{ isCopy: boolean }>();
 </script>
 
 <template>
   <div
-    class="fixed bg-bg outline outline-secondary text-additional p-4 rounded-xl space-x-2 duration-250 transition-all origin-right bottom-8"
+    class="fixed bg-bg outline outline-secondary text-additional p-4 rounded-xl space-x-2 duration-250 transition-all origin-right bottom-8.5 z-1"
     :class="isCopy ? 'scale-100 right-8' : 'scale-60 -right-40'"
   >
     <CopyIcon class="size-4 inline-block shrink-0 mb-1" />
